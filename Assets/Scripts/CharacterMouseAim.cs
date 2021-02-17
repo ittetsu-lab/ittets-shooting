83a0cc45ca7e03f476aa314027780f8f3276f0e6
miilab <sakuta.deth@gmail.com>
2021-02-18 00:48:13 +0900

    右JoyStickによって水平方向に視点移動できる機能追加 (Asset StoreからJoyStickアセットをインストール)

--- Assets/Scripts/CharacterMouseAim.cs
+++ Assets/Scripts/CharacterMouseAim.cs
@@ -6,8 +6,8 @@ public class CharacterMouseAim : MonoBehaviour
 {
 	public Transform ver_Rotation; // 親オブジェクト(player本体)のTransformを指定
  	public Transform hor_Rotation; // CameraオブジェクトのTransformを指定
-    public float ver_CameraRate = 5.0F; // 水平方向のカメラ転換度合
-    public float hor_CameraRate = 1.0F; // 垂直方向のカメラ転換度合
+    public float ver_CameraRate = 5.0F; // 垂直方向の視点移動度合
+    public float hor_CameraRate = 1.0F; // 水平方向の視点移動度合
 
     // Start is called before the first frame update
     void Start()
